22e947fce685d68ab8cb6e1faae64896c9a3a191
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2023-10-12 16:02:18 -0600

    functions ok

--- src/matriz.py
+++
@@ -1,7 +0,0 @@
-class Matriz:
-    def __init__(self, nombre):
-        self.renglones = renglones
-        self.columnas = columnas
-        self.matriz = [[0 for _ in range(columnas)] for _ in range(renglones)]
-
-
